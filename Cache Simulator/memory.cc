918b4b4dd861e9f7b71f69d6850aca772ad81ae1
Linna <1500012782@pku.edu.cn>
2017-12-11 14:43:16 +0800

    Change the memory time calculating.

--- Cache Simulator/memory.cc
+++ Cache Simulator/memory.cc
@@ -4,7 +4,7 @@ void Memory::HandleRequest(uint64_t addr, int bytes, int read,
 {
 	stats_.access_counter++;
 	hit = 1;
-	time = time + latency_.hit_latency + latency_.bus_latency;
+	time = latency_.hit_latency + latency_.bus_latency;
 	stats_.access_time += latency_.hit_latency + latency_.bus_latency;
 }
 
